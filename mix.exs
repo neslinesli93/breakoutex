e99e1e3aa641c151a9660358819784823e1e623e
Tommaso <p.tommy93@gmail.com>
2019-03-31 22:24:58 +0200

    Update elixir version to 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Demo.Mixfile do
     [
       app: :demo,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
