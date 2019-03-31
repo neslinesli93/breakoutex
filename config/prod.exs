734c34cf52ddb6721e24313b8ee8069b20cbb39c
Tommaso <p.tommy93@gmail.com>
2019-03-31 22:32:16 +0200

    Add env variable for PORT

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :demo, DemoWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "fierce-river-58732", port: 443],
+  url: [scheme: "https", host: "fierce-river-58732", port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
