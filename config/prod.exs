087293c15c5d121569edeef82a2113d7927e9879
Tommaso <p.tommy93@gmail.com>
2019-03-31 22:41:02 +0200

    Disable check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,8 @@ config :demo, DemoWeb.Endpoint,
   url: [scheme: "https", host: "fierce-river-58732", port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  check_origin: false
 
 # Do not print debug messages in production
 config :logger, level: :info
