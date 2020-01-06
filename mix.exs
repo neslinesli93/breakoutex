b3b04928690f9532dccdd166a9242539000f5d08
Tommaso <p.tommy93@gmail.com>
2020-01-06 16:40:21 +0100

    Fix broken layout by mistake

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Breakoutex.Mixfile do
   def project do
     [
       app: :breakoutex,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
