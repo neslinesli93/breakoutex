8ef9e3847c1df95da089d12fadf6dcc41321b3fe
Tommaso <p.tommy93@gmail.com>
2020-01-06 16:23:56 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Breakoutex.Mixfile do
   def project do
     [
       app: :breakoutex,
-      version: "0.2.0",
+      version: "0.3.1",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
