091a3f5d82339cd35fed73362385a2184c17881c
Mikk <mikk@heathmont.net>
2020-12-21 16:03:07 +0200

    origin fix

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :breakoutex, BreakoutexWeb.Endpoint,
   version: Application.spec(:phoenix_distillery, :vsn),
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  check_origin: ["https://breakoutex.tommasopifferi.com"]
+  check_origin: ["https://cgbreakout.gigalixirapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :debug
